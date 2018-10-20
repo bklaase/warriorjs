3761116df01ab460266cd8579c15b8e8d91dd9d9
Bob <psilioxus@haplo.localdomain>
2018-10-20 19:28:20 +0200

    level 3 completed

--- psilioxus-baby-steps/Player.js
+++ psilioxus-baby-steps/Player.js
@@ -1,6 +1,29 @@
 class Player {
-  playTurn(warrior) {
-    // Cool code goes here.
-    warrior.walk();
+
+  // constructor
+  constructor()  {
+    this.health = 20;
+    this.resting = false;
+  }
+
+
+  playTurn(warrior) {        
+    const feelings = warrior.feel();     
+    const health = warrior.health();
+    const enemy = feelings && feelings.getUnit() && feelings.getUnit().isEnemy();
+    const hurtInPreviousTurn = (health < this.health);
+    const criticallyInjured = health < 15;
+
+    this.health = warrior.health();
+    
+    if(enemy)
+      warrior.attack();
+     else if(hurtInPreviousTurn) // taking dmg from range, charge!
+       warrior.walk('forward');
+     else if(criticallyInjured) //injured, but no incomming dmg, rest until sufficient health
+       warrior.rest();
+    else //nothing ... so just advance!
+      warrior.walk();
   }
 }
+
